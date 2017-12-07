0c0e7ae5b4266a662e5366bb9be3ae3a61ea1ac7
yayanheeh <tokoxp58@gmail.com>
2017-12-07 19:59:20 +0700

    Update config.js

--- js/lib/config.js
+++ js/lib/config.js
@@ -59,32 +59,16 @@ Config.I18n = {
   locale: 'id-id',
   supported: [
     'id-id'
-    , 'en-us'
-    , 'es-es'
-    , 'de-de'
-    , 'it-it'
-    , 'nl-nl'
-    , 'pt-br'
+    , 
   // ,"ru-ru"
   ], // To be copied to package.json
   languages: {
     'id-id': 'Indonesia',
-    'en-us': 'English',
-    'de-de': 'Deutsch',
-    'es-es': 'Español',
-    'it-it': 'Italiano',
-    'ru-ru': 'Русский',
-    'nl-nl': 'Nederlands',
-    'pt-br': 'Português (Brazil)'
+   
   },
   aliases: {
     'id': 'id-id',
-    'en': 'en-us',
-    'de': 'de-de',
-    'es': 'es-es',
-    'it': 'it-it',
-    'ru': 'ru-ru',
-    'nl': 'nl-nl'
+    
   },
   messages: {},
   fallback_messages: {}
